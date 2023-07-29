c7dfb5056744506c4982ed3fcb758c3c0ca6da66
Mohit Chawla <chawlamohit45@gmail.com>
2023-07-30 03:06:21 +0530

    added posters

--- main.py
+++ main.py
@@ -23,10 +23,13 @@ from movie_wiki import extract_movie_plot
 from movie_info2 import plot
 from script_cohere import scripter
 from voice import voiceover
+from poster import get_poster
 import time
 import os
 
 def main():
+    header_image = "header_image.jpg"
+    st.image(header_image, use_column_width=True)
     st.title("Movie AudioBook Generator")
     name = st.text_input("Enter the name of the movie:")
     if st.button("Generate Audio"):
@@ -36,16 +39,18 @@ def main():
             movie_plot = movie_plot + extract_movie_plot(name)
         # st.write(movie_plot)
         # with st.spinner("Generating audio..."):
-            
+        get_poster(movie_data['Title'])
         progress_bar = st.progress(0)
         script = scripter(movie_data['Title'], movie_data['Year'], movie_data['Genre'], movie_plot)
+
+        # st.write(script)
+        # st.write(movie_data['Genre'])
+        voiceover(script, movie_data['Title'], movie_data['Genre'])
+
         for i in range(100):
             time.sleep(0.1)
             progress_bar.progress(i + 1)
         st.success("Audio generated successfully!")
-        # st.write(script)
-        # st.write(movie_data['Genre'])
-        # voiceover(script, movie_data['Title'], movie_data['Genre'])
         audio_file = open(f'audio_generated/{movie_data["Title"]}.mp3', 'rb')
         audio_bytes = audio_file.read()
 
@@ -56,10 +61,14 @@ def main():
         # Display each audio file in a loop
     for audio_file in audio_files:
         if audio_file.endswith(".mp3"):
+
             audio_name = os.path.splitext(audio_file)[0]
+
+
+            movie_poster = f"posters\{audio_name}.jpg"
+            st.image(movie_poster, width=200)
             st.write(f"**{audio_name}**")
             st.audio(open(os.path.join("audio_generated", audio_file), 'rb').read(), format='audio/mp3')
-
 
 
 
---
+++ poster.py
@@ -0,0 +1,13 @@
+import movieposters
+import requests
+# Get the link to the poster for "Jackie Brown"
+
+def get_poster(Title):
+    poster_url = movieposters.get_poster(Title)
+
+    # Download the poster
+    response = requests.get(poster_url)
+    with open(f"posters\{Title}.jpg", "wb") as f:
+        f.write(response.content)
+
+# get_poster("")
